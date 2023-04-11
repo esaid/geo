4901420b91f07a3b03d43f53e60233ba98677991
esaid <emmanuel.said@cern.ch>
2023-04-11 22:21:35 +0200

    ajout _map modification icon man changement couleur button update_map current_center init_state Permission timer detabase .env apiKey zoomIn_step pos_latitutde pos_longitude to detabase

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -20,7 +20,7 @@ class _HomePageState extends State<HomePage> {
   MapController mapController = MapController();
   latLng.LatLng currentCenter = latLng.LatLng(0.0, 0.0);
   Position? position;
-  double zoomIn_step = 0.20;
+  final double  zoomIn_step = 0.20;
   double pos_latitutde = 0.0;
   double pos_longitude = 0.0;
 
@@ -118,7 +118,9 @@ class _HomePageState extends State<HomePage> {
 
   void _zoom() {
     print('current  Zoom :  $currentZoom');
-    (currentZoom >= 18) ? currentZoom = 18 : currentZoom = currentZoom + zoomIn_step;
+    (currentZoom >= 18)
+        ? currentZoom = 18
+        : currentZoom = currentZoom + zoomIn_step;
 
     mapController.move(currentCenter, currentZoom);
   }
